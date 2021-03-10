dd59c121758d6b5764d0c0448ef758274a02f4c1
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-03-10 23:19:04 +0300

    Utility functions

---
+++ SDC_Includes/utility_functions.php
@@ -0,0 +1,50 @@
+<?php
+/***********************************************************************************/
+/*                          (C) 2021 - Skyfallen                                   */
+/*                  Skyfallen Developer Center Developed by                        */
+/*                          The Skyfallen Company                                  */
+/*                                                                                 */
+/*   This file contains some utility functions that help with the development.     */
+/***********************************************************************************/
+
+// Check if called from the main file
+defined("SDC_ABSPATH") or die("Stop it!");
+
+/**
+ * Echos the WEB_URL of current application
+ */
+function the_weburl(){
+    echo WEB_URL;
+}
+
+/**
+ * Echos the url concatenated with an page's url
+ * @param String $path Path to the page
+ */
+function the_pageurl($path){
+    echo WEB_URL.$path;
+}
+
+/**
+ * Echos the url concatenated with an asset's url
+ * @param String $path Path to the url inside the app
+ */
+function the_fileurl($path){
+    echo WEB_URL.$path;
+}
+
+/**
+ * Redirects the user using headers
+ * @param String $to The path after the domain to redirect to
+ */
+function sdc_redirect($to){
+    header("location: ".WEB_URL.$to);
+}
+
+/**
+ * Generates a random MD5 Hash
+ * @return String Hash
+ */
+function rand_md5_hash(){
+    return md5(uniqid(rand(), true));
+}
