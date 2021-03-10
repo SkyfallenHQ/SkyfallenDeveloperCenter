82cbc8b85a3c983ef3690029a717b44c777a1c5d
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-03-10 23:17:18 +0300

    Topbar

---
+++ partials/top-menu.php
@@ -0,0 +1,32 @@
+<?php
+/***********************************************************************************/
+/*                          (C) 2021 - Skyfallen                                   */
+/*                  Skyfallen Developer Center Developed by                        */
+/*                          The Skyfallen Company                                  */
+/*                                                                                 */
+/*                      This file contains the top menu                            */
+/***********************************************************************************/
+
+// Check if called from the main file
+defined("SDC_ABSPATH") or die("Direct access is not allowed.");
+?>
+<div class="top-menu-wrap">
+    <a id="menu-toggle-a" href="javascript:toggleNav()"><svg id="menu-toggle" viewBox="0 -53 384 384" width="384pt" xmlns="http://www.w3.org/2000/svg"><path d="m368 154.667969h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 32h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/><path d="m368 277.332031h-352c-8.832031 0-16-7.167969-16-16s7.167969-16 16-16h352c8.832031 0 16 7.167969 16 16s-7.167969 16-16 16zm0 0"/></svg></a>
+    <select id="org-select" class="org-selector">
+        <?php
+
+        $uo = $_SESSION['user']->getUserOrganisations();
+
+        foreach ($uo as $org){
+
+            echo "<option>".$org->obj->orgname." (".$org->obj->identifier.")</option>";
+
+        }
+
+        ?>
+    </select>
+    <button class="profile-circle" id="profile-circle" onclick="openPage('<?php the_weburl(); ?>profile')">
+
+        <svg style="height: 20px; width: 20px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+    </button>
+</div>
