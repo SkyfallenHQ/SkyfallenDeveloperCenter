b2411d92bc0b8d5c7e902771856251c335ce5b49
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-03-10 23:17:30 +0300

    Router

---
+++ router.php
@@ -0,0 +1,45 @@
+<?php
+/***********************************************************************************/
+/*                          (C) 2021 - Skyfallen                                   */
+/*                  Skyfallen Developer Center Developed by                        */
+/*                          The Skyfallen Company                                  */
+/*                                                                                 */
+/*         This file is handles all url requests and redirects them.               */
+/***********************************************************************************/
+
+// Check if our ABSPATH is defined
+defined("SDC_ABSPATH") or die("Don't mess!");
+
+// Start routing all urls
+
+include_once SDC_ABSPATH."/views/login_view.php";
+SDC_Router::routePage("accounts/login","render_login");
+SDC_Router::routePage("logout","do_logout", true,"redirect_to_login");
+
+include_once SDC_ABSPATH."/views/home.php";
+SDC_Router::routePage("/","render_dev_home", true,"redirect_to_login");
+
+include_once SDC_ABSPATH."/views/docs.php";
+SDC_Router::routePage("docs","render_dev_docs", true,"redirect_to_login");
+
+include_once SDC_ABSPATH."/views/profile.php";
+SDC_Router::routePage("profile","render_dev_profile", true,"redirect_to_login");
+
+include_once SDC_ABSPATH."/views/apps.php";
+SDC_Router::routePage("apps","render_dev_apps", true,"redirect_to_login");
+
+include_once SDC_ABSPATH."/views/id_login.php";
+SDC_Router::routePage("developerid","do_oauth_init");
+SDC_Router::routePage("developerid/callback","handle_oauth_callback");
+
+include_once SDC_ABSPATH."/views/svc_notavailable.php";
+include_once SDC_ABSPATH."/views/apphome.php";
+SDC_Router::routePrefix("apps/manage","render_app_page", true,true,"redirect_to_login");
+
+// If nothing was routed, display 404
+if(!defined("ROUTED")){
+
+    // Include the 404 Page.
+    include_once SDC_ABSPATH."/SDC_Includes/404.php";
+
+}
