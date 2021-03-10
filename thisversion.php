4f0d35ed27ab18322c99dd67cf4d62feb2a9fbf7
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-03-10 23:19:43 +0300

    Version storage file

---
+++ thisversion.php
@@ -0,0 +1,5 @@
+<?php
+define("THIS_VERSION","SFR-401B00");
+define("THIS_VERSION_NICKNAME", "IDS");
+define("THIS_VERSION_BRANCH","Internal Development Sample");
+define("VERSION_PROVIDER","Skyfallen Developers");
