d948f122bc4cc2d5b2a47223f36db083091c709c
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-03-10 23:17:55 +0300

    CSRF Class

---
+++ SDC_FunctionSets/SDC_CSRF.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Class sdc_CSRF
+ * Creates CSRF Tokens for Forms
+ */
+class SDC_CSRF
+{
+    /**
+     * @var String $CSRF_ID Stores the CSRF Token's ID that we are using
+     */
+    private $CSRF_ID;
+
+    /**
+     * @var String $CSRF_TOKEN Stores the CSRF Token that we are using
+     */
+    private $CSRF_TOKEN;
+
+    /**
+     * SSF_CSRF constructor.
+     * Creates a new CSRF Token
+     */
+    public function __construct(){
+
+        $this->CSRF_ID = rand_md5_hash();
+
+        $this->CSRF_TOKEN = rand_md5_hash();
+
+        $_SESSION["csrf_codes"][$this->CSRF_ID] = $this->CSRF_TOKEN;
+    }
+
+    /**
+     * Checks if the CSRF from the form is valid.
+     * @return bool false if invalid CSRF, true on valid CSRF
+     */
+    public static function verifyCSRF(){
+
+        if(isset($_POST["csrf_id"]) && isset($_POST["csrf_token"])) {
+            $form_csrf_id = $_POST["csrf_id"];
+
+            $form_csrf_token = $_POST["csrf_token"];
+
+            if (isset($_SESSION["csrf_codes"][$form_csrf_id]) && (@$_SESSION["csrf_codes"][$form_csrf_id] == $form_csrf_token)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Renders two hidden inputs containing CSRF Tokens
+     */
+    public function put(){
+        ?>
+            <input type="password" value="<?php echo $this->CSRF_ID ?>" name="csrf_id" hidden>
+            <input type="password" value="<?php echo $this->CSRF_TOKEN ?>" name="csrf_token" hidden>
+        <?php
+    }
+
+    /**
+     * Clears out all previous CSRF Tokens
+     */
+    public static function clearTokens(){
+
+        $_SESSION["csrf_codes"] = array();
+
+    }
+
+    /**
+     * This function will invalidate the current CSRF
+     */
+    public static function invalidateCurrentCSRF(){
+
+        unset($_SESSION[$_POST["csrf_id"]]);
+
+    }
+}
