1d8aa7a626717971054909feb74585a4408cf23d
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-03-11 21:21:11 +0300

    Update AppServer Main

--- appserver/main.py
+++ appserver/main.py
@@ -1,1 +1,16 @@
-# Backend for SDC
+#  ____  _           __       _ _            #
+# / ___|| | ___   _ / _| __ _| | | ___ _ __  #
+# \___ \| |/ / | | | |_ / _` | | |/ _ \ '_ \ #
+#  ___) |   <| |_| |  _| (_| | | |  __/ | | |#
+# |____/|_|\_\\__, |_|  \__,_|_|_|\___|_| |_|#
+#             |___/                          #
+###########################################################################
+#                   (C) 2021 - Skyfallen Developers                       #
+#                 This file handles the main process                       #
+###########################################################################
+
+from processmain import SDCAppServer
+
+if __name__ == '__main__':
+    SDCAppServerInstance = SDCAppServer()
+    SDCAppServerInstance.HandleAppServer()
