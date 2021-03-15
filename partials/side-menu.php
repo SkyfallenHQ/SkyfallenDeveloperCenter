75cf2fc97ba06e1ca888b659b91523f848e68727
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-03-15 23:33:24 +0300

    Update side-menu

--- partials/side-menu.php
+++ partials/side-menu.php
@@ -22,10 +22,32 @@ defined("SDC_ABSPATH") or die("Direct access is not allowed.");
     <div class="side-menu-wrapper">
         <ul class="side-menu">
             <li>
-                <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>apps')">My Apps</button>
-                <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>docs')">Developer Docs</button>
-                <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>profile')">My Profile</button>
-                <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>license')">License Agreement</button>
+                <?php
+                    if($_SESSION['currentOrg']->obj->orgtype == "ORGANISATION"){
+
+                        ?>
+
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>apps')"><?php echo $_SESSION['currentOrg']->obj->orgname ?>'s Apps</button>
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>docs')">Developer Docs</button>
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>profile')">My Profile</button>
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>organisations/profile')">Organisation Profile</button>
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>organisations/members')">Team Members</button>
+
+                        <?php
+
+                    } else {
+
+                        ?>
+
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>apps')">My Apps</button>
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>docs')">Developer Docs</button>
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>profile')">My Profile</button>
+                        <button class="side-menu-btn" onclick="openPage('<?php the_weburl(); ?>license')">License Agreement</button>
+
+                        <?php
+
+                    }
+                ?>
             </li>
         </ul>
     </div>
